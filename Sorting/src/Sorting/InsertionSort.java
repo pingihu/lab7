d272a88f29a2b6195de67722b68ced6f78eb34eb
Meryl Charleston <meryl_charleston@brown.edu>
2015-04-07 19:03:49 -0400

    wrote insertionSort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -8,8 +8,15 @@ public class InsertionSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
+		for (int i = 0; i < unsortedArray.length; i++) {
+		  int j = i;
+		  while (j > 0 && unsortedArray[j - 1] > unsortedArray[j]) {
+		    int temp = unsortedArray[j];
+		    unsortedArray[j] = unsortedArray[j - 1];
+		    unsortedArray[j - 1] = temp;
+		    j = j - 1;
+		  }
+		}
+		return unsortedArray;
 	}
 }
--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -21,12 +21,11 @@ public class Main {
 		System.out.println();
 		
 		System.out.println("Sorted Array:");
-		//TODO: sort the array and print out its contents
-		int[] sortedArray = BubbleSort.bubbleSort(unsortedArray);
-		for (int i : sortedArray) {
-			System.out.print(i + " ");
+
+		int[] sortedArray = InsertionSort.insertionSort(unsortedArray);
+		for (int i: sortedArray) {
+      System.out.print(i + " ");
 		}
-		
 	}
 
 }
