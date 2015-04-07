554bd2d8880e2c2bd7cbd466c7f3a442d79671e3
Ping Hu <ping_hu@brown.edu>
2015-04-07 18:57:43 -0400

    Implement Bubble sort, the bubbliest of sorts...

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -1,5 +1,17 @@
 package Sorting;
 public class BubbleSort {
+	
+	private static boolean isSorted(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
 	/**
 	 * bubbleSort				sorts an array of integers using bubble sort
 	 * 
@@ -8,7 +20,19 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		int n = unsortedArray.length;
+		boolean swapped = false;
+		do {
+		  swapped = false;
+		  for (int i = 1; i < n; i++) {
+		    if (unsortedArray[i - 1] > unsortedArray[i]) {
+			    int temp = unsortedArray[i];
+			    unsortedArray[i] = unsortedArray[i - 1];
+			    unsortedArray[i - 1] = temp;
+			    swapped = true;
+		    }
+		  }
+		} while (!isSorted(unsortedArray));
+		return unsortedArray;
 	}
 }
